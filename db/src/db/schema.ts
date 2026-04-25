e836582418d06c9cedce5da565af496c50145103
João Antonio Alem <67281961+JoaoAlem@users.noreply.github.com>
2026-04-25 16:01:37 -0300

    :bug: Fix | adicionar colunas de data no SQL (#2)

--- db/src/db/schema.ts
+++ db/src/db/schema.ts
@@ -1,8 +1,11 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255}).notNull()
+  password: varchar({ length: 255}).notNull(),
+  createdAt: timestamp({ mode: "date" }).defaultNow(),
+  updatedAt: timestamp({ mode: "date" }).defaultNow().$onUpdateFn(() => new Date()),
+  deletedAt: timestamp({ mode: "date" })
 })
